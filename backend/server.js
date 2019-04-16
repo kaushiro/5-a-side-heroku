9bca9c424d455caebb5bc2458f893d3e3482d7cc
Kim Williams <kim@Kims-MacBook-Air.local>
2019-04-16 16:42:38 +0100

    modified ports and mongodb url

--- backend/server.js
+++ backend/server.js
@@ -14,9 +14,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(logger("dev"));
 // this is our MongoDB database
-// const dbRoute = "mongodb+srv://kaushiro:hihello@cluster0-mjdf4.mongodb.net/test?retryWrites=true";
+const dbRoute = process.env.MONGO_URL || "mongodb+srv://kaushiro:hihello@cluster0-mjdf4.mongodb.net/test?retryWrites=true";
 // connects our back end code with the database
-const dbRoute = 'mongodb://127.0.0.1:27017/players';
+// const dbRoute = 'mongodb://127.0.0.1:27017/players';
 mongoose.connect(
   dbRoute,
   { useNewUrlParser: true }
