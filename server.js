73af7cf52c9c42097e15a44d8ef124e5ffeb24c2
Kim Williams <kim@Kims-MacBook-Air.local>
2019-04-16 21:50:04 +0100

    config variables

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 const mongoose = require("mongoose");
 const express = require("express");
 // const router = express.Router();
+require("dotenv").config();
 var cors = require('cors');
 const bodyParser = require("body-parser");
 const logger = require("morgan");
 const playersAPIRouter = require("./routes/playersAPI");
 const path = require("path");
 const app = express();
-
+app.use(express.static(path.join(__dirname, 'client/build')));
 // this is our MongoDB database
 // const dbRoute = process.env.MONGODB_URI || "mongodb+srv://kaushiro:hihello@cluster0-mjdf4.mongodb.net/test?retryWrites=true";
 // connects our back end code with the database
+const API_PORT = process.env.PORT || 3001;
 const dbRoute = process.env.MONGODB_URI ||'mongodb://127.0.0.1:27017/players';
 mongoose.connect(
   dbRoute,
@@ -24,17 +26,17 @@ db.once("open", () => console.log("connected to the database"));
 
 // checks if connection with the database is successful
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
-const API_PORT = process.env.PORT || 3001;
+
 app.set('port', API_PORT);
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(logger("dev"));
-app.use(express.static(path.join(__dirname, "client", "build")));
+
 //using the routes I created in /routes/playersAPI
 app.use("/players", playersAPIRouter);
 // launch our backend into a port\
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 app.listen(API_PORT, () => console.log(`LISTENING ON PORT ${API_PORT}`));
